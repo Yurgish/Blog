44ba602f7d65bda61c629faeabc77cec017f5cb4
Yurgish <humenyuriipetrovych@gmail.com>
2024-07-14 13:32:33 +0300

    Added policy CORS to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes";
+
 dotenv.config();
 const { MONGO_URL, APP_PORT } = process.env;
 
 const app: Express = express();
 const PORT = APP_PORT || 5000;
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 
