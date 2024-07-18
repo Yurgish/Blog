ca04965064ed43317163ea702e15795ba36b1899
Yurgish <humenyuriipetrovych@gmail.com>
2024-07-18 14:14:29 +0300

    Added icons

---
+++ client/src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileIcon from "./ProfileIcon";
+
+const Menu = () => {
+    return (
+        <div className="flex flex-col w-24 border-r-2 border-green px-24">
+            <ProfileIcon login="plaseholder" />
+        </div>
+    );
+};
+
+export default Menu;
