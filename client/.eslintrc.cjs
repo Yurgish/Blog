97f6c0fece6de89331ffdbd5022df129983db746
Yurgish <humenyuriipetrovych@gmail.com>
2024-08-19 16:05:42 +0300

    configured absolute imports, added react testing library

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+    settings: {
+        react: {
+            version: "detect", // Automatically detect the React version
+        },
+    },
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: 2020, // Allows modern ECMAScript features
+        sourceType: "module", // Allows import/export statements
+        ecmaFeatures: {
+            jsx: true, // Enables JSX parsing
+        },
+    },
+};
